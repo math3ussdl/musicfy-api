0938037d42fe901017067cec083ef430630101d8
limab <limabrot879@gmail.com>
2024-05-05 10:47:56 -0300

    chore: setup project

---
+++ src/main/java/io/webx/musicfyapi/MusicfyApiApplication.java
@@ -0,0 +1,12 @@
+package io.webx.musicfyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicfyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicfyApiApplication.class, args);
+    }
+}
---
+++ src/test/java/io/webx/musicfyapi/MusicfyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.webx.musicfyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicfyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
